e68532104963f4fa60e2b8ea7d34596529118249
Jacob Park <park.jacob.96@gmail.com>
2019-01-14 12:41:19 -0500

    Added hooks/post_gen_project.sh

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+#
+# Functions:
+#
+
+function echo_info() {
+	echo "[cookiecutter-scala-play] INFO post_gen_project.sh - $1" >&1
+}
+
+
+#
+# Step 1:
+#
+
+echo_info "Initializing Git"
+git init
+git add .
+git commit -n -m "Initial Commit"
+
+#
+# Step 2:
+#
+
+echo_info "Initializing Project"
+make init
+
+#
+# Done:
+#
+
+echo_info "Done!"
